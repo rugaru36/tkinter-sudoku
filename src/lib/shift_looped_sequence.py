9341a0db99f959e160991682734e6261db49e66b
Anton Lebedev <antn.lbdv@yandex.ru>
2025-07-29 10:37:56 +0300

    lib dir commit fix

---
+++ src/lib/shift_looped_sequence.py
@@ -0,0 +1,5 @@
+def shift_looped_sequence(sequence: list[int], offset: int = 0):
+    if offset <= 0: return sequence
+    first_part = sequence[:offset]
+    second_part = sequence[offset:]
+    return second_part + first_part
