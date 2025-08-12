56ddd88ede0a53c4bb67b5727261ae967938069c
antn.lbdv@yandex.ru <=>
2025-08-12 14:29:47 +0300

    difficulty update

--- src/domain/difficulty.py
+++ src/domain/difficulty.py
@@ -13,13 +13,13 @@ class Difficulty:
                 return {
                     "name": name,
                     "time_seconds": 15 * 60,
-                    "count_of_unknown_elements": 15,
+                    "count_of_unknown_elements": 20,
                     "count_of_mistakes": 10}
             case Difficulty.mid:
                 return {
                     "name": name,
                     "time_seconds": 10 * 60,
-                    "count_of_unknown_elements": 25,
+                    "count_of_unknown_elements": 30,
                     "count_of_mistakes": 7
                 }
             case Difficulty.hard:
